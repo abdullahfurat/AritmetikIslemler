697642897f65565d175bb62366304239f4cfb8f4
muratvuranok <muratvuranok@hotmail.com>
2019-10-27 11:57:09 +0300

    Add project files.

---
+++ AritmetikIslemler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AritmetikIslemler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Disaridan alinan iki sayının toplamiyla farkinin birbirine bolumunden kalanin sonucu kactir? (Farkın toplama bolumunden kalan kactir?)
+
+        }
+    }
+}
